46a559808436fd03e6d1a01f24bda2890819f2a0
monmonplaza <monmon.plaza@gmail.com>
2022-01-31 14:25:48 +0800

    123

--- src/components/pages/home/students/StudentsTable.js
+++ src/components/pages/home/students/StudentsTable.js
@@ -5,8 +5,12 @@ import NoData from "../../../nodata/NoData";
 import BannerCreateButton from "../../../widgets/BannerCreateButton";
 import SearchBar from "../../../widgets/SearchBar";
 import ShowMore from "../../../widgets/ShowMore";
-
+import { studentData } from "../../../data/StudentData.js";
 const StudentTable = () => {
+  let count = 0;
+
+  console.log(studentData);
+  console.log(studentData.length);
   return (
     <>
       <section className="content">
@@ -40,216 +44,41 @@ const StudentTable = () => {
                   </thead>
 
                   <tbody>
-                    <tr className="">
-                      <td colSpan="100%">
-                        <NoData />
-                      </td>
-                    </tr>
-                    <tr className="red-critical">
-                      <td>1</td>
-                      <td>Temporary</td>
-                      <td></td>
-                      <td>Mendoza</td>
-                      <td>Dan Josepth</td>
-                      <td>N/A</td>
-                      <td>2021-02-12</td>
-                      <td>
-                        <div className="dropdown">
-                          <button className=" table--btn t--red">
-                            <BsFillGearFill />
-                            <IoMdArrowDropdown />
-                          </button>
-                          <div className="dropdown-link">
-                            <a href="/students">Edit</a>
-                            <a href="#">Delete</a>
-                            <a href="#">View Grades</a>
-                          </div>
-                        </div>
-                      </td>
-                    </tr>
-
-                    <tr className="red-critical">
-                      <td>1</td>
-                      <td>Temporary</td>
-                      <td></td>
-                      <td>Ubeda</td>
-                      <td>Alexander</td>
-                      <td>N/A</td>
-                      <td>2021-02-12</td>
-                      <td>
-                        <div className="dropdown">
-                          <button className=" table--btn t--red">
-                            <BsFillGearFill />
-                            <IoMdArrowDropdown />
-                          </button>
-                          <div className="dropdown-link">
-                            <a href="/students">Edit</a>
-                            <a href="#">Delete</a>
-                            <a href="#">View Grades</a>
-                          </div>
-                        </div>
-                      </td>
-                    </tr>
-
-                    <tr className="red-critical">
-                      <td>1</td>
-                      <td>Temporary</td>
-                      <td>45854724196954</td>
-                      <td>Ubeda</td>
-                      <td>Alexander</td>
-                      <td>N/A</td>
-                      <td>2021-02-12</td>
-                      <td>
-                        <div className="dropdown">
-                          <button className=" table--btn t--red">
-                            <BsFillGearFill />
-                            <IoMdArrowDropdown />
-                          </button>
-                          <div className="dropdown-link">
-                            <a href="/students">Edit</a>
-                            <a href="#">Delete</a>
-                            <a href="#">View Grades</a>
-                          </div>
-                        </div>
-                      </td>
-                    </tr>
-
-                    <tr>
-                      <td>1</td>
-                      <td>Official</td>
-                      <td>4585474596954</td>
-                      <td>Ubeda</td>
-                      <td>Alexander</td>
-                      <td>N/A</td>
-                      <td>2021-02-12</td>
-                      <td>
-                        <div className="dropdown">
-                          <button className=" table--btn t--red">
-                            <BsFillGearFill />
-                            <IoMdArrowDropdown />
-                          </button>
-                          <div className="dropdown-link">
-                            <a href="/students">Edit</a>
-                            <a href="#">Delete</a>
-                            <a href="#">View Grades</a>
-                          </div>
-                        </div>
-                      </td>
-                    </tr>
-
-                    <tr>
-                      <td>1</td>
-                      <td>Official</td>
-                      <td>44326546196954</td>
-                      <td>Ubeda</td>
-                      <td>Alexander</td>
-                      <td>N/A</td>
-                      <td>2021-02-12</td>
-                      <td>
-                        <div className="dropdown">
-                          <button className=" table--btn t--red">
-                            <BsFillGearFill />
-                            <IoMdArrowDropdown />
-                          </button>
-                          <div className="dropdown-link">
-                            <a href="/students">Edit</a>
-                            <a href="#">Delete</a>
-                            <a href="#">View Grades</a>
-                          </div>
-                        </div>
-                      </td>
-                    </tr>
-
-                    <tr>
-                      <td>1</td>
-                      <td>Official</td>
-                      <td>51245268634554</td>
-                      <td>Ubeda</td>
-                      <td>Alexander</td>
-                      <td>N/A</td>
-                      <td>2021-02-12</td>
-                      <td>
-                        <div className="dropdown">
-                          <button className=" table--btn t--red">
-                            <BsFillGearFill />
-                            <IoMdArrowDropdown />
-                          </button>
-                          <div className="dropdown-link">
-                            <a href="/students">Edit</a>
-                            <a href="#">Delete</a>
-                            <a href="#">View Grades</a>
-                          </div>
-                        </div>
-                      </td>
-                    </tr>
-                    <tr className="red-critical">
-                      <td>1</td>
-                      <td>Temporary</td>
-                      <td>51245268634554</td>
-                      <td>Ubeda</td>
-                      <td>Alexander</td>
-                      <td>N/A</td>
-                      <td>2021-02-12</td>
-                      <td>
-                        <div className="dropdown">
-                          <button className=" table--btn t--red">
-                            <BsFillGearFill />
-                            <IoMdArrowDropdown />
-                          </button>
-                          <div className="dropdown-link">
-                            <a href="/students">Edit</a>
-                            <a href="#">Delete</a>
-                            <a href="#">View Grades</a>
-                          </div>
-                        </div>
-                      </td>
-                    </tr>
-
-                    <tr>
-                      <td>1</td>
-                      <td>Official</td>
-                      <td>51245268634554</td>
-                      <td>Ubeda</td>
-                      <td>Alexander</td>
-                      <td>N/A</td>
-                      <td>2021-02-12</td>
-                      <td>
-                        <div className="dropdown">
-                          <button className=" table--btn t--red">
-                            <BsFillGearFill />
-                            <IoMdArrowDropdown />
-                          </button>
-                          <div className="dropdown-link">
-                            <a href="/students">Edit</a>
-                            <a href="#">Delete</a>
-                            <a href="#">View Grades</a>
-                          </div>
-                        </div>
-                      </td>
-                    </tr>
-
-                    <tr className="red-critical">
-                      <td>1</td>
-                      <td>Temporary</td>
-                      <td>51245268634554</td>
-                      <td>Ubeda</td>
-                      <td>Alexander</td>
-                      <td>N/A</td>
-                      <td>2021-02-12</td>
-                      <td>
-                        <div className="dropdown">
-                          <button className=" table--btn t--red">
-                            <BsFillGearFill />
-                            <IoMdArrowDropdown />
-                          </button>
-                          <div className="dropdown-link">
-                            <a href="/students">Edit</a>
-                            <a href="#">Delete</a>
-                            <a href="#">View Grades</a>
-                          </div>
-                        </div>
-                      </td>
-                    </tr>
+                    {studentData.length > 0 ? (
+                      studentData.map((item, key) => {
+                        count += 1;
+                        return (
+                          <tr className="red-critical">
+                            <td>{count}</td>
+                            <td>{item.enrollment_status}</td>
+                            <td>{item.lrn}</td>
+                            <td>{item.first_name}</td>
+                            <td>{item.last_name}</td>
+                            <td>{item.grade}</td>
+                            <td>{item.regdate}</td>
+                            <td>
+                              <div className="dropdown">
+                                <button className=" table--btn t--red">
+                                  <BsFillGearFill />
+                                  <IoMdArrowDropdown />
+                                </button>
+                                <div className="dropdown-link">
+                                  <a href="/students">Edit</a>
+                                  <a href="#">Delete</a>
+                                  <a href="#">View Grades</a>
+                                </div>
+                              </div>
+                            </td>
+                          </tr>
+                        );
+                      })
+                    ) : (
+                      <tr className="">
+                        <td colSpan="100%">
+                          <NoData />
+                        </td>
+                      </tr>
+                    )}
                   </tbody>
                 </table>
               </div>
