085060164b05702904961a08d4fb35d49a7ea729
monmonplaza <monmon.plaza@gmail.com>
2022-01-31 14:08:36 +0800

    123

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -19,7 +19,7 @@ const Home = () => {
       {/* <ModalCreateStudent /> */}
       {/* <ModalDeleteStudent /> */}
       {/* <ModalSuccess /> */}
-      <Spinner />
+      {/* <Spinner /> */}
       <StudentTable />
     </main>
   );
